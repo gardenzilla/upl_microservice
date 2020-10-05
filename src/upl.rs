73a00b869a4f0db34376b8d5521bd061c0997905
Peter <mezeipetister@gmail.com>
2020-10-05 13:34:16 +0200

    ..

--- src/upl.rs
+++ src/upl.rs
@@ -23,6 +23,8 @@ enum Status {
   Opened(u32),
 }
 
+type ScrapingId = String;
+
 pub struct Upl {
   id: UplId,
   sku: SKU,
@@ -31,9 +33,12 @@ pub struct Upl {
   net_retail_price: f32,                 // ?
   net_retail_price_custom: Option<f32>,  // ?
   custom_price_history: Vec<UplCPH>,     // ?
-  can_devide: Option<u32>,               // ?
+  can_divide: Option<u32>,               // ?
   best_before: Option<DateTime<Utc>>,    // todo: DateTime<Utc>!
   location_history: Vec<UplHistoryItem>, // todo: + DateTime per event. Only location change?
+  inherited_from: Option<UplId>,         // If it's derived from another UPL
+  is_opened: bool,                       // If it's opened
+  has_damage: Option<ScrapingId>,        // If damaged, we need to enlcose the related scrape doc id
 }
 
 pub struct UplCPH {
@@ -46,4 +51,58 @@ pub struct UplHistoryItem {
   created_at: DateTime<Utc>,
   created_by: UserId,
   location: UplLocation,
+}
+
+pub mod New {
+  use crate::prelude::*;
+  use std::collections::HashMap;
+  type SKU = String;
+  pub struct UplPhdr {
+    upl_id: u32,
+    sku: SKU,
+  }
+
+  enum LocationKind {
+    Stock,
+    Cart,
+    Delivery,
+    Customer,
+  }
+  struct Location {
+    id: u32,
+    kind: LocationKind,
+    upl_store: HashMap<SKU, Vec<UplPhdr>>,
+  }
+
+  struct UplLocations {
+    stock: Vec<Location>,
+    cart: Vec<Location>,
+    delivery: Vec<Location>,
+  }
+
+  impl UplLocations {
+    pub fn move_upl(
+      &mut self,
+      from_kind: &str,
+      from_id: u32,
+      to_kind: &str,
+      to_id: u32,
+      sku: &SKU,
+      upl_id: u32,
+    ) -> ServiceResult<()> {
+      let mut from_place = match from_kind {
+        "stock" => &mut self.stock,
+        "cart" => &mut self.cart,
+        "delivery" => &mut self.delivery,
+        _ => return Err(ServiceError::not_found("Given from kind not found")),
+      };
+      let mut to_place = match to_kind {
+        "stock" => &mut self.stock,
+        "cart" => &mut self.cart,
+        "delivery" => &mut self.delivery,
+        _ => return Err(ServiceError::not_found("Given to kind not found")),
+      };
+      Ok(())
+    }
+  }
 }
